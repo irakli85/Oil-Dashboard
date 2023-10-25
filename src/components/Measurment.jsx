c7ec6b60256b8dff26a753b4c78aabff317c236b
iberidze38 <iberidze38@gmail.com>
2023-10-25 12:30:24 +0400

    first commit

--- src/components/Measurment.jsx
+++ src/components/Measurment.jsx
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 const Measurment = () => {
   return (
     <Container>
-        <H2>ნავთობისა და ნავთობპროდუცტების რაოდენობის გაზომვის მეთოდების დასაშვები ცდომილების ნორმები</H2>
+        <H2>ნავთობისა და ნავთობპროდუქტების რაოდენობის გაზომვის მეთოდების დასაშვები ცდომილების ნორმები</H2>
     </Container>
   )
 }
@@ -19,7 +19,7 @@ const Container = styled.div`
 `
 
 const H2 = styled.h2`
-  font-size: 3.4rem;
+  font-size: 2.4rem;
   font-weight: 900;
   width: 125rem;
   text-align: center;
