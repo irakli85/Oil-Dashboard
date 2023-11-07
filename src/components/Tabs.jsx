e6bf0b93df372b0f4ab5eda1a8b509e4ead5668d
iberidze38 <iberidze38@gmail.com>
2023-11-07 13:10:58 +0400

    first commit

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import bot from '../assets/bot.png'
+import vibro from '../assets/vibro.png'
+import ter1 from '../assets/ter1.png'
+
+function Tabs() {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+
+  return (
+    <div>
+      <TabBtnDiv>
+        <TabButton index={0} activeTab={activeTab} onClick={handleTabClick}>
+            <Img src={bot} alt="bot" />
+          Batumi Oil Terminal
+        </TabButton>
+        <TabButton index={1} activeTab={activeTab} onClick={handleTabClick}>
+          <Img src={vibro} alt="vibro" />
+          Vibro Diagnostik
+        </TabButton>
+        <TabButton index={2} activeTab={activeTab} onClick={handleTabClick}>
+        <Img src={ter1} alt="terminal1" />
+          Terminal 1
+        </TabButton> 
+      </TabBtnDiv>
+
+      <div>
+        {activeTab === 0 && <BOT />}
+        {activeTab === 1 && <Vibro />}
+        {activeTab === 2 && <Terminal1 />}
+      </div>
+    </div>
+  );
+}
+
+function TabButton({ index, activeTab, onClick, children }) {
+  const isActive = activeTab === index;
+  return (
+    <TabBtn
+      onClick={() => onClick(index)}
+      className={isActive ? 'active' : ''}      
+    >
+      {children}
+    </TabBtn>
+  );
+}
+
+function BOT() {
+  return <div>Lorem</div>;
+}
+
+function Vibro() {
+  return <div>Tab 2 Content</div>;
+}
+
+function Terminal1() {
+    return <div>Tab 3 Content</div>;
+  }
+
+export default Tabs;
+
+const TabBtnDiv = styled.div`
+    display: flex;
+    gap: 0.5rem;
+    margin-top: 3rem;
+`
+
+const TabBtn = styled.button`
+    font-family: 'Poppins';
+    width: 30rem;
+    font-size: 2rem;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    cursor: pointer;
+    border: 3px solid gray;
+    border-radius: 5px 5px 0 0;
+    padding: 1rem;  
+`
+
+const Img = styled.img`
+    width: 4rem;
+    height: 4rem;
+`
--- src/components/Terminals.jsx
+++ src/components/Terminals.jsx
@@ -1,5 +1,7 @@
-import { Container } from './Measurment'
+import { Container, Hr, H2 } from './Measurment'
 import styled from 'styled-components'
+import Tabs from './Tabs'
+import bg from '../assets/bot1.jpg'
 
 const Terminals = () => {
 
@@ -7,10 +9,44 @@ const Terminals = () => {
 
     return (
     <Container>
-        asdasd
+        <Header>
+          <H2st>ნავთობ-ტერმინალები</H2st>
+        </Header>
+        <Hr/>        
+        <Tabs />        
     </Container>
   )
 }
+
+const Header = styled.div`
+  width: 100%;
+  border-radius: 1.5rem;
+  height: 30rem;
+  background-image: url(${bg});  
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  padding: 3rem;
+  position: relative;
+  &::after{
+    content: '';
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0,0,0, 0.2);
+    border-radius: 1.5rem;
+  }
+`
+
+const H2st = styled(H2)`
+  color: #fff;
+  font-size: 3.4rem;
+  position: relative;
+  z-index: 2;
+  font-weight: 900;
+`
 
 
 
